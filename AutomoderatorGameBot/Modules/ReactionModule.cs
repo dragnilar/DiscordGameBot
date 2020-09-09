ae7face8206ee5b2098303037ccfba8a2d737fa8
Stephen White <stephenjeffreywhite@outlook.com>
2020-09-09 01:54:42 -0400

    Bugfix for reaction module

--- AutomoderatorGameBot/Modules/ReactionModule.cs
+++ AutomoderatorGameBot/Modules/ReactionModule.cs
@@ -26,9 +26,12 @@ namespace AutomoderatorGameBot.Modules
             var reactions = GetReactions().Where
                 (x => e.Message.Content.ToLower().Contains(x.ReactKeyword)).ToList();
             if (!reactions.Any()) return;
+            var reactionCount = 0;
             foreach (var emoji in reactions.Select(reaction => DiscordEmoji.FromName(client, reaction.ReactionEmojiCode)))
             {
                 await e.Message.CreateReactionAsync(emoji);
+                reactionCount++;
+                if (reactionCount == 20) break; //Discord caps reactions at 20, so stop if there's 20.
             }
         }
     }
